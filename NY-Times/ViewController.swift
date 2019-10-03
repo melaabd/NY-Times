12778323cff924eedc0105a5e68e2f3d76ed4db1
Mohamad El-Abd <mohamd.elaabd@gmail.com>
2019-10-03 16:17:11 +0400

    Initial Commit

---
+++ NY-Times/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NY-Times
+//
+//  Created by Alfoah IOS on 03/10/2019.
+//  Copyright © 2019 El-Abd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
